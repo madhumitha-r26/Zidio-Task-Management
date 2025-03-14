ebff8e3b97e88f79e6f12f8cbc8d5fc31e734a16
madhumitha-ravichandran26 <madhumitha26301@gmail.com>
2025-03-14 12:31:51 +0530

    deployment at backend

--- server/index.js
+++ server/index.js
@@ -1,39 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const app = express();
 const cors = require("cors");
-const cookieParser=require("cookie-parser")
+const cookieParser = require("cookie-parser");
 
+dotenv.config();
 const DbConnection = require("./DbConnection");
 DbConnection();
 
-dotenv.config();
+const app = express();
 
 app.use(cors({
-  origin: "https://zidio-task-management-two.vercel.app",  
-  credentials: true,  
+  origin: "https://zidio-task-management-two.vercel.app",
+  credentials: true,
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization"
 }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://zidio-task-management-two.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://zidio-task-management-two.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.sendStatus(200);
-});
-
-
-app.use(cookieParser());
 app.use(express.json());
+app.use(cookieParser());
 
 const userRouter = require("./router/userRouter");
 const taskRouter = require("./router/taskRouter");
@@ -41,10 +25,14 @@ const taskRouter = require("./router/taskRouter");
 app.use("/users", userRouter);
 app.use("/tasks", taskRouter);
 
-app.get('/',(req,res)=>{
-  res.status(200).json({message:"Zidio Task Management"})
-})
-
-app.listen(process.env.PORT, () => {
-  console.log(`SERVER IS RUNNING ON PORT - ${process.env.PORT}`);
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Zidio Task Management" });
 });
+
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`SERVER IS RUNNING ON PORT - ${process.env.PORT}`);
+// });
+
+// Instead of app.listen, export the app as a handler
+module.exports = app;
